f80bdce3ed4e6aaf2666f92a83029cd24514e7ab
aashishsatya <ankarathaashish@gmail.com>
2016-02-13 03:13:54 +0530

    added code to fetch reviews

--- app/src/main/java/com/example/android/popularmoviesapp_p1/MovieDetailFragment.java
+++ app/src/main/java/com/example/android/popularmoviesapp_p1/MovieDetailFragment.java
@@ -1,6 +1,7 @@
 package com.example.android.popularmoviesapp_p1;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,6 +13,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -19,6 +21,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 import retrofit.RestAdapter;
@@ -58,6 +66,11 @@ public class MovieDetailFragment extends Fragment {
                 // convert the String extra back into JSON
                 JSONObject movieDetailsJSON = new JSONObject(movieDetails);
 
+                // this is for debugging
+                // TODO: remove this code later
+
+                Log.d(LOG_TAG + "movieID:", movieDetailsJSON.getString(TAG_MOVIE_ID));
+
                 // get all the required views
 
                 TextView title_textView = (TextView) rootView.findViewById(R.id.title_textView);
@@ -66,10 +79,6 @@ public class MovieDetailFragment extends Fragment {
                 TextView year_textView = (TextView) rootView.findViewById(R.id.year_textView);
                 TextView ratings_textView = (TextView) rootView.findViewById(R.id.ratings_textView);
                 review_listView = (ListView) rootView.findViewById(R.id.review_listview);
-                if (review_listView == null) {
-                    Log.d(LOG_TAG, "ListView Adapter is null");
-                }
-
 
                 // load them up
 
@@ -96,19 +105,28 @@ public class MovieDetailFragment extends Fragment {
         RestAdapter restAdapter;
 
         String API_URL = "http://api.themoviedb.org/3/movie/";
+        String API_PARAM = "api_key";
         String TAG_AUTHOR = "author";
         String TAG_CONTENT = "content";
+        String TAG_RESULTS = "results";
 
         @Override
         protected void onPreExecute() {
         }
 
-        @Override
+        /*@Override
         protected JSONArray doInBackground(String... params) {
             String movieID = params[0];
             API_URL = API_URL + movieID;
             restAdapter = new RestAdapter.Builder()
                     .setEndpoint(API_URL)
+                    .setLogLevel(RestAdapter.LogLevel.FULL)
+                    .setLog(new RestAdapter.Log() {
+                        @Override
+                        public void log(String msg) {
+                            Log.i(LOG_TAG, msg);
+                        }
+                    })
                     .build();
             ReviewsAPI reviewsAPI = restAdapter.create(ReviewsAPI.class);
             JSONObject reviewsJSONObject = reviewsAPI.getMovieReviews(BuildConfig.MOVIE_DB_API_KEY);
@@ -120,6 +138,91 @@ public class MovieDetailFragment extends Fragment {
                 e.printStackTrace();
             }
             return reviewsJSONArray;
+        }*/
+
+        @Override
+        protected JSONArray doInBackground(String... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String reviewsJSONStr = null;
+
+            try {
+
+                // code referenced from Udacity's Sunshine app
+
+                // Construct the URL for the MovieDB query
+                // Possible parameters are avaiable at MovieDB's forecast API page, at
+                // http://docs.themoviedb.apiary.io/#reference/discover
+
+                // build the new URL
+
+                Uri builtUri = Uri.parse(API_URL + params[0] + "/reviews")
+                        .buildUpon()
+                        .appendQueryParameter(API_PARAM, BuildConfig.MOVIE_DB_API_KEY)
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                // Create the request to MovieDB, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                reviewsJSONStr = buffer.toString();
+                // obtain and return the data
+                try {
+                    JSONObject reviewsJSONObject = new JSONObject(reviewsJSONStr);
+                    Log.d(LOG_TAG, reviewsJSONObject.toString());
+                    JSONArray reviewJSONArray = reviewsJSONObject.getJSONArray(TAG_RESULTS);
+                    // successfully obtained movie data from JSON string
+                    // now return this value
+                    return reviewJSONArray;
+                } catch (JSONException e) {
+                    // can't get movie data from obtained string
+                    // nothing to do
+                    return null;
+                }
+            } catch (IOException e) {
+                // If the code didn't successfully get the movie data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Toast.makeText(getActivity(), "Error closing data stream", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
         }
 
         @Override
@@ -161,7 +264,7 @@ public class MovieDetailFragment extends Fragment {
                 e.printStackTrace();
             }
 
-            return "Name: " + authorName + "\n" + "Content: " + content + "\n";
+            return "Name: " + authorName + "\n" + "Review: " + content + "\n";
         }
     }
 }
